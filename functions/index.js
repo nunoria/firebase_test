ff7e6954c17db49ff93644d0951e0bd2af014dfa
nunoria <kissyou7@gmail.com>
2022-11-04 15:30:21 +0900

    first commit

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+
+exports.bigben = functions.https.onRequest((req, res) => {
+  const hours = (new Date().getHours() % 12) + 1; // London is UTC + 1hr;
+  const date = new Date();
+  console.log("시간을 바꿔:" + date);
+  res.status(200).send(`<!doctype html>
+      <head>
+        <title>Time</title>
+      </head>
+      <body>
+        ${"BONG ".repeat(hours)}
+      </body>
+    </html>`);
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
